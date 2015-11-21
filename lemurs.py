756e2fb0a7f176e9c987d7c3078c7ba237098df4
Jonah Ryan-Davis <jonah.ryandavis@gmail.com>
2015-11-21 15:26:05 -0500

    Create table from input data

--- lemurs.py
+++ lemurs.py
@@ -6,9 +6,33 @@ import fileinput
 def main(argv=None):
 	if argv is None:
 		argv=sys.argv
-	print argv
+	createTable()
+	
+def readLines():
 	for line in fileinput.input():
-		print line
+		return line
+
+def readColumns(columnstring, column_list, table):
+	for column_name in columnstring.split("\t"):
+		column_list.append( column_name)
+		table[column_name] = {}
+
+def readRow(rowstring, column_list, table):
+	rowsplit = rowstring.split("\t")
+	patient_name = rowsplit.pop(0)
+	for i in range(0, len(rowsplit)):
+		table[column_list[i]][patient_name] = rowsplit[i]
+
+def createTable():
+	table = {}
+	filestring = readLines()
+	filesplit = filestring.split("\r")
+	column_list = []
+	columnstring = filesplit.pop(0)
+	readColumns(columnstring, column_list, table)
+	for rowstring in filesplit:
+		readRow(rowstring, column_list, table)
+	print table
 
 if __name__ == "__main__":
 	sys.exit(main())
