960712e41de6e2f448a54ef9f9c9e34b585f21b5
Jonah Ryan-Davis <jonah.ryandavis@gmail.com>
2015-11-21 14:40:40 -0500

    Formatted trainingData.txt and lemurs.py reads from stdin

--- lemurs.py
+++ lemurs.py
@@ -1,10 +1,14 @@
 #!/bin/python
 
 import sys
+import fileinput
 
 def main(argv=None):
 	if argv is None:
 		argv=sys.argv
+	print argv
+	for line in fileinput.input():
+		print line
 
 if __name__ == "__main__":
 	sys.exit(main())
